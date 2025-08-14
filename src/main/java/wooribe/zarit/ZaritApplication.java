ff0b1cb0f8bf2082dd7520dcd41e33c008638df3
hyennnnnny <shw030507@naver.com>
2025-08-14 15:32:11 +0900

    feat: Initial commit

---
+++ src/main/java/wooribe/zarit/ZaritApplication.java
@@ -0,0 +1,13 @@
+package wooribe.zarit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaritApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaritApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wooribe/zarit/ZaritApplicationTests.java
@@ -0,0 +1,13 @@
+package wooribe.zarit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaritApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
