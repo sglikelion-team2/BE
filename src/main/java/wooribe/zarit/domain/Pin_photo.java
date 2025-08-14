20d16863c159ac50040a70283094e2944bb67eee
Jeonhaechan <101245130+Jeonhaechan@users.noreply.github.com>
2025-08-14 18:43:49 +0900

    feat: pin_photo

--- src/main/java/wooribe/zarit/domain/Pin_photo.java
+++ src/main/java/wooribe/zarit/domain/Pin_photo.java
@@ -1,4 +1,22 @@
 package wooribe.zarit.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "pin_photo")
 public class Pin_photo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // 독립적인 기본 키(PK)
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pin_id") // 외래 키(FK) 컬럼 지정
+    private Pin pin;
+
+    private String photo; // 사진 URL
 }
