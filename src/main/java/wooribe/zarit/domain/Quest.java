c52d8112495162f7ee246f2fa20cf63df1708d8e
Jeonhaechan <101245130+Jeonhaechan@users.noreply.github.com>
2025-08-14 17:24:55 +0900

    feat:domainì생성-User,Quest,User_preference,Pin_plugbar,noise,wifi

--- src/main/java/wooribe/zarit/domain/Quest.java
+++ src/main/java/wooribe/zarit/domain/Quest.java
@@ -1,4 +1,22 @@
 package wooribe.zarit.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "quest")
 public class Quest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //01234까지 시작시 테이블 만들어져잇어야할듯
+    @Column(name = "quest_id")
+    private Long id;
+
+    @Column(name = "quest_info")
+    private String questInfo;
+
+    private int reward = 0; // 기본값 0으로 설정
 }
---
+++ src/main/java/wooribe/zarit/domain/enums/Atmos.java
@@ -0,0 +1,10 @@
+package wooribe.zarit.domain.enums;
+
+public enum Atmos{
+    QUIET,      // 조용
+    LIVELY,     // 활기
+    CHILL,      // 췰~
+    RandB,       // R&B
+    LOFI,        // LO-FI
+    UNCONCERNED // 신경안씀
+}
