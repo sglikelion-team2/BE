174e659659f64db20bb02970a5b3c2914db37af0
hyennnnnny <shw030507@naver.com>
2025-08-17 22:07:36 +0900

    fix: response 내용 수정 및 예외 처리 수정

--- src/main/java/wooribe/zarit/dto/response/ApiResponse.java
+++ src/main/java/wooribe/zarit/dto/response/ApiResponse.java
@@ -1,5 +1,8 @@
 package wooribe.zarit.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -7,13 +10,18 @@ import lombok.Getter;
 @Getter
 @Builder
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({ "isSuccess", "code", "message", "result" })
 public class ApiResponse <T>{
-    private boolean isSuccess;
+
+    @JsonProperty("isSuccess")
+    private Boolean isSuccess;
+
     private int code;
     private String message;
     private T result;
 
-    public ApiResponse(boolean success, int code, String message) {
+    public ApiResponse (boolean success, int code, String message) {
         this.isSuccess = success;
         this.code = code;
         this.message = message;
